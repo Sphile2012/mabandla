02b70214e8f88b368e63c2653db6ec70404b995a
Sphile2012 <poomeigh503@gmail.com>
2026-05-16 04:03:49 +0200

    fix: add ForgotPassword, ResetPassword, and ChangePassword routes

--- src/pages.config.js
+++ src/pages.config.js
@@ -50,9 +50,11 @@
 import About from './pages/About';
 import AdminUpload from './pages/AdminUpload';
 import Categories from './pages/Categories';
+import ChangePassword from './pages/ChangePassword';
 import CompleteProfile from './pages/CompleteProfile';
 import DownloadApp from './pages/DownloadApp';
 import Favorites from './pages/Favorites';
+import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Messages from './pages/Messages';
@@ -61,6 +63,7 @@ import PaymentSuccess from './pages/PaymentSuccess';
 import Pricing from './pages/Pricing';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
+import ResetPassword from './pages/ResetPassword';
 import StudentDashboard from './pages/StudentDashboard';
 import VideoPlayer from './pages/VideoPlayer';
 import __Layout from './Layout.jsx';
@@ -70,9 +73,11 @@ export const PAGES = {
     "About": About,
     "AdminUpload": AdminUpload,
     "Categories": Categories,
+    "ChangePassword": ChangePassword,
     "CompleteProfile": CompleteProfile,
     "DownloadApp": DownloadApp,
     "Favorites": Favorites,
+    "ForgotPassword": ForgotPassword,
     "Home": Home,
     "Login": Login,
     "Messages": Messages,
@@ -81,6 +86,7 @@ export const PAGES = {
     "Pricing": Pricing,
     "Profile": Profile,
     "Register": Register,
+    "ResetPassword": ResetPassword,
     "StudentDashboard": StudentDashboard,
     "VideoPlayer": VideoPlayer,
 }
